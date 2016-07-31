4482e38389ca68897f5bb2e822e63ea1c327d90f
SaviorEdge <savioredge11@gmail.com>
2016-07-31 06:10:08 -0500

    Trying to fix cc (#2)

--- commands/custom-commands.js
+++ commands/custom-commands.js
@@ -4,6 +4,6 @@
 exports.commands = {
 		cc:'cc',
 	cc:function() {
-		this.splitReply(Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | ^^out of order^^ | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9)
+		this.restrictReply('Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | ^^out of order^^ | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9')
 	},
 };
