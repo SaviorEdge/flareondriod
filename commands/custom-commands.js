2afb7aa42a513195da652f0fbbc720a16ba5d64e
SaviorEdge <savioredge11@gmail.com>
2016-08-05 21:45:38 -0400

    Update custom-commands.js

--- commands/custom-commands.js
+++ commands/custom-commands.js
@@ -4,6 +4,6 @@
 exports.commands = {
 		cc:'cc',
 	cc:function() {
-		this.restrictReply('Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | Cancer')
+		this.restrictReply('Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9')
 	},
 };
