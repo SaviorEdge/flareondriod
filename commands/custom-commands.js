82bd0b83e2dae4503a4ffa341a27aca190d8c621
SaviorEdge <savioredge11@gmail.com>
2016-08-14 15:08:57 -0600

    added dank

--- commands/custom-commands.js
+++ commands/custom-commands.js
@@ -5,6 +5,6 @@ exports.commands = {
 		cc:'cc',
 	cc:function() {
 		this.restrictReply('Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave')
-		this.restrictReply('Custom Command Continued: hi/hey | jump | btahb |  ')
+		this.restrictReply('Custom Command Continued: hi/hey | jump | btahb | dank | ')
 	},
 };
