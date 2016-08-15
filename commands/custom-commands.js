27981bf17f9561aaec874bd87d84699b30e13022
SaviorEdge <savioredge11@gmail.com>
2016-08-15 17:21:30 -0600

    Update custom-commands.js

--- commands/custom-commands.js
+++ commands/custom-commands.js
@@ -2,7 +2,9 @@
 	Custom Commands
 */
 exports.commands = {
-		cc:'cc',
+		commands: 'cc',
+		commandlist: 'cc',
+		cmds:'cc',
 	cc:function() {
 		this.restrictReply('Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave')
 		this.restrictReply('Custom Command Continued: hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666')
