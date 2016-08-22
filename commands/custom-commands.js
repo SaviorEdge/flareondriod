63c847fe87626fdf1612a402444dd7f017f34b34
SaviorEdge <savioredge11@gmail.com>
2016-08-22 11:31:57 -0500

    Added calm

--- commands/custom-commands.js
+++ commands/custom-commands.js
@@ -7,6 +7,6 @@ exports.commands = {
 		cmds:'cc',
 	cc:function() {
 		this.restrictReply('Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave')
-		this.restrictReply('Custom Command Continued: hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | ')
+		this.restrictReply('Custom Command Continued: hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | ')
 	},
 };
