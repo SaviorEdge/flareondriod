bebabce062f484565268bffd77f1708af9120f79
SaviorEdge <savioredge11@gmail.com>
2016-10-28 13:16:23 -0500

    Added harambeharrison

--- commands/custom-commands.js
+++ commands/custom-commands.js
@@ -6,7 +6,7 @@ exports.commands = {
 	commandlist: 'cc',
 	cmds:'cc',
 	cc:function() {
-		this.reply('!htmlbox <div style="background-color: #333; color: #ff9933; border: 3px solid #ff9933; font-family: comicsansms,ubuntu;"><center><font size=5>Custom Commands</font><br><div style="width: 95%"><font size=3>Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random or tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave | hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded | overcomb</font></div><br></center></div>');
+		this.reply('!htmlbox <div style="background-color: #333; color: #ff9933; border: 3px solid #ff9933; font-family: comicsansms,ubuntu;"><center><font size=5>Custom Commands</font><br><div style="width: 95%"><font size=3>Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random or tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave | hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded | overcomb | harambeharrison</font></div><br></center></div>');
 	},
 };
 /*
