6044a1182f862c3ad833693c1264350903f828df
SaviorEdge <savioredge11@gmail.com>
2016-10-28 10:26:49 -0500

    Added trumpdab :3

--- commands/custom-commands.js
+++ commands/custom-commands.js
@@ -7,6 +7,11 @@ exports.commands = {
 		cmds:'cc',
 	cc:function() {
 		this.restrictReply('Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave')
-		this.restrictReply('Custom Command Continued: hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded')
+		this.restrictReply('Custom Command Continued: hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded | trumpdab')
 	},
 };
+/*
+Maybe I should switch to htmlbox...
+Pretty sure that's more efficient than this lol
+Eh I'll do it later.
+*/
