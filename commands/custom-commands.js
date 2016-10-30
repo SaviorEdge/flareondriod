bcc1744bd7faaabb3a62468ac7b36fc3ba4e7eca
SaviorEdge <savioredge11@gmail.com>
2016-10-29 19:19:03 -0500

    Adding nemo since it was requested so many times...

--- commands/custom-commands.js
+++ commands/custom-commands.js
@@ -6,13 +6,13 @@ exports.commands = {
 	commandlist: 'cc',
 	cmds:'cc',
 	cc:function() {
-		this.reply('!htmlbox <div style="background-color: #333; color: #ff9933; border: 3px solid #ff9933; font-family: comicsansms,ubuntu;"><center><font size=5>Custom Commands</font><br><div style="width: 95%"><font size=3>Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random or tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave | hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded | overcomb | harambeharrison | squiddab | greendance | bravo | chickenfame | dog | smile</font></div><br></center></div>');
+		this.reply('!htmlbox <div style="background-color: #333; color: #ff9933; border: 3px solid #ff9933; font-family: comicsansms,ubuntu;"><center><font size=5>Custom Commands</font><br><div style="width: 95%"><font size=3>Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random or tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave | hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded | overcomb | harambeharrison | squiddab | greendance | bravo | chickenfame | dog | smile | nemo</font></div><br></center></div>');
 	},
 };
 /*
 Yes, I've switched from the old text-based code to something more "clean".
 Don't like it?
-Old code below just for convinence. This will NOT be updated, so you're going to have to do this manually. Unless the one of the servers I use it on doesn't allow bots to use !htmlbox. 
+Old code below just for convinence. This will NOT be updated, so you're going to have to do this manually. Unless the one of the servers I use it on doesn't allow bots to use !htmlbox.
 	this.restrictReply('Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave')
 	this.restrictReply('Custom Command Continued: hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded | overcomb')
 */
