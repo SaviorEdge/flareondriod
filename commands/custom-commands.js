0aac0381b1e67ee385349e4dd317b9a28186ac7a
Edge <nicholasthds@gmail.com>
2017-08-05 16:56:14 -0500

    Redo cc for options w/o htmlbox

--- commands/custom-commands.js
+++ commands/custom-commands.js
@@ -2,6 +2,45 @@
 	Custom Commands
 */
 exports.commands = {
+        cc:function(arg,by,room)
+    {
+        var a=arg.toLowerCase();//var text = this.hasRank(by, '+%@#&~') || room.charAt(0) === ',' ? '' : '/pm ' + by + ', ';
+        var text = "";
+        if(a==="")
+        {
+					this.reply('Please use **list** (must be global bot, roomleader/globalleader, admin, or roomowner to show) __or__ **text** (Gives you a message like this for each command) after the command.')
+        }
+        else if(a==="list")
+        {
+					this.reply('!htmlbox <div style="background-color: #000; color: #fff; border: 3px solid #fff; font-family: comicsansms,ubuntu;"><center><font size=5>Custom Commands</font><br><div style="width: 95%"><font size=2>Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random or tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Dance | Punish | Boof | Lopunny | 9 | summon - leave | hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | calm | poliswag | chill | ded | overcomb | harambeharrison | squiddab | greendance | bravo | chickenfame | dog | smile | nemo | books | nword | lost | bnow | shady | plan | highnoot | nootfather | nooty | comingtotheaters | duel | faze | fire | annoy</font></div><br></center></div>');
+        }
+        else if(a==="text")
+        {
+					this.restrictReply('Custom Commands (1): Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave'),
+					this.restrictReply('Custom Commands (2): hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded | overcomb'),
+					this.restrictReply('Custom Commands (3): harambeharrison | squiddab | greendance | bravo | chickenfame | dog | smile | nemo | books | nword | lost | bnow | shady | plan | highnoot | nootfather | nooty | comingtotheaters | duel | faze | fire | annoy');
+        }
+/* ---Might be used later, links to ghostbin for a list.---
+			else if(a==="link")
+        {
+            this.reply('this shouldn't be able to show up...report to bot maintainer please');
+        }
+*/
+        if(by.charAt(0)=='+' || by.charAt(0)=='%' || by.charAt(0)=='@' || by.charAt(0)=='#' || by.charAt(0)=='&' || by.charAt(0)=='~')
+        {
+        this.say(room,text);
+        }
+        else
+        {
+        this.pmReply (text);
+        }
+
+    },
+};
+/*
+//			Old code, reuse if problems occur (there shouldn't lol). New code hasnt been tested yet, so you might want to use this if you don't trust me, which I don't blame you, I don't trust myself.
+//
+exports.commands = {
 	commands: 'cc',
 	commandlist: 'cc',
 	cmds:'cc',
@@ -13,8 +52,9 @@ exports.commands = {
 Yes, I've switched from the old text-based code to something more "clean".
 Don't like it?
 Old code below just for convinence. This will NOT be updated, so you're going to have to do this manually. Unless the one of the servers I use it on doesn't allow bots to use !htmlbox.
-	this.restrictReply('Custom Commands: Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave')
-	this.restrictReply('Custom Command Continued: hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded | overcomb')
+	this.restrictReply('Custom Commands (1): Boney | Potato | Eevee | Beerus | DarkEevee | Die | TPoll (random | tier) | DankTrump | Pawwz | db | eggsandlegs | Cena | Pikachu | Diancie | Flareon | Sans | God | WhoAmI | Whereisedge | Dance | Punish | Boof | Lopunny | 9 | summon - leave')
+	this.restrictReply('Custom Commands (2): hi/hey | jump | btahb | dank | dankharambe | numel | vapenation | green | windows | dancing | youareanidiot | face | 7 | cell | 666 | bucks | temmie | afk - back | spheal | bnowlin | calm | poliswag | chill | ded | overcomb')
+	this.restrictReply('Custom Commands (3): harambeharrison | squiddab | greendance | bravo | chickenfame | dog | smile | nemo | books | nword | lost | bnow | shady | plan | highnoot | nootfather | nooty | comingtotheaters | duel | faze | fire | annoy')
 */
 
 
